12e354f1cc103e093f7bd426e68f928f9e48adae
Jorge Luiz Edward <jorge.edward@amedsaude.com.br>
2026-06-01 22:03:03 -0400

    a

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
-  cloudflare: false, // Isso precisa estar aqui para desligar o build da Cloudflare
+  base: "./", // Garante que o app procure os assets na pasta certa, independente de onde for hospedado
+  cloudflare: false,
   plugins: [netlify()],
   tanstackStart: {
     server: { entry: "server" },
